30441ccee3601845b226864a3541cf93690aaa97
Corey Silver <coreysilver@outlook.com>
2023-01-18 14:29:43 -0500

    refactored code so that client connection is in a separate file

--- play.js
+++ play.js
@@ -1,20 +1,2 @@
 const net = require("net");
-
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  })
-
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-  console.log(data)
-  });
-
-  return conn;
-}
-
-console.log("Connecting ...");
-connect();
+const { connect } = require("./client.js");
